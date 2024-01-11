0ca1bbdc6e62eaff61252c23108995c145341f65
johnmartinroque <150325822+johnmartinroque@users.noreply.github.com>
2024-01-11 17:53:59 +0800

    change in footer

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link to="/home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
             <Nav.Link href="#/cart"><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
