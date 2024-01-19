aa10aaf2aff1d63457c978bca7afd2d3beb3f0ce
Mikaela Yumang <myyumang@student.hau.edu.ph>
2024-01-19 12:13:58 +0800

    search function

--- frontend/src/screens/Homepage.jsx
+++ frontend/src/screens/Homepage.jsx
@@ -1,12 +1,49 @@
 // Homepage.js
-import React from 'react';
+
 import Header from '../components/Header';
 
+import React, { useState, useEffect } from 'react';
+import jsonData from './sampledata.json';
+import '../style.css';
+
 function Homepage() {
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    setData(jsonData);
+  }, []);
+
+  useEffect(() => {
+    const filtered = data.filter((item) =>
+      item.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredData(filtered);
+  }, [data, searchTerm]);
+
   return (
     <>
-      <Header />
-      <div>Homepage Content</div>
+    <Header />
+      <h3 className='text-center my-3'>Search for Sharers you love!</h3>
+      <div className="searchBox">
+            <input
+              id="searchInput"
+              className="searchInput"
+              type="text"
+              placeholder="Search here..."
+              onChange={(event) => setSearchTerm(event.target.value)}
+            />
+            <div className="templateContainer">
+              {filteredData.map((item) => (
+                <div className="template" key={item.id}>
+                  <img src={item.image} alt={item.name} />
+                  <h3>{item.name}</h3>
+                  <p>{item.bio}</p>
+                </div>
+              ))}
+            </div>
+          </div>
     </>
   );
 }
