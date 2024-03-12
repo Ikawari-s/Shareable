0cca1ef86f4884d741c7206645f978f85bc07f4a
Ikawari-s <lnmanese@student.hau.edu.ph>
2024-03-13 01:59:26 +0800

    linis ng header, ung routing gawin ko siyang isang page lang

--- frontend/src/components/GuestHeader.jsx
+++ frontend/src/components/GuestHeader.jsx
@@ -33,12 +33,12 @@ function GuestHeader() {
     <>
       <header id="bodhi" className={isScrolled ? 'bodhi circular' : 'bodhi'}>
           <nav className="d-flex justify-content-center">
-            <Nav.Link style={{ color: 'gray', fontWeight: ''}} as={Link} to="/">
-              <img src={logotext} alt="Logo" />
+            <Nav.Link className="d-flex justify-content-center" style={{ color: 'gray', fontWeight: ''}} as={Link} to="/">
+              <img src={logotext} alt="Logo" id='logotext' />
             </Nav.Link>
-            <Nav.Link style={{ color: 'gray', fontWeight: '', padding: 'none' }} as={Link} to="/userprofile">FIND A SHARER</Nav.Link>
-            <Nav.Link style={{ color: 'gray', fontWeight: ''}} as={Link} to="/login">LOG IN</Nav.Link>
-            <Nav.Link style={{ color: 'gray', fontWeight: ''}} as={Link} to="/signup">SIGN UP (going insane)</Nav.Link>
+            <Nav.Link className="d-flex justify-content-center" style={{ color: 'gray', fontWeight: '', paddingTop: '1rem', fontWeight: '600'}} as={Link} to="/userprofile">FIND A SHARER</Nav.Link>
+            <Nav.Link className="d-flex justify-content-center" style={{ color: 'gray', fontWeight: '', paddingTop: '1rem', fontWeight: '600'}} as={Link} to="/login">LOG IN</Nav.Link>
+            <Nav.Link className="d-flex justify-content-center" style={{ color: 'gray', fontWeight: '', paddingTop: '1rem', fontWeight: '600'}} as={Link} to="/signup">SIGN UP</Nav.Link>
           </nav>
 
       </header>
