5346f7c14b7fac424d49730d610cde83b0a68e48
Ikawari-s <lnmanese@student.hau.edu.ph>
2024-03-19 02:05:06 +0800

    signup pati login fix just in case

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -3,6 +3,8 @@ import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { login } from "../actions/userActions";
 import GuestHeader from "../components/GuestHeader";
+import logotext from '../designs/images/logotext.png';
+import logo from '../designs/images/logo.png';
 
 function Login() {
   const dispatch = useDispatch();
@@ -38,11 +40,12 @@ function Login() {
   }, [userInfo, navigate]);
 
   return (
+    <div id="LogInPage">
+      <div id="gab"> <img src={logotext} alt="Logo" /></div>
     <div className="container mt-5">
-      <GuestHeader />
-      <div className="card custom-card-background">
+      <div className="card custom-card-background wrapper">
         <div className="card-header">
-          <h3 className="text-center">Log in</h3>
+          <h1 className="text-center">Log in</h1>
         </div>
 
         <div className="card-body">
@@ -53,6 +56,7 @@ function Login() {
               </label>
               <input
                 type="email"
+                placeholder="Email"
                 className="form-control"
                 id="email"
                 name="email"
@@ -66,6 +70,7 @@ function Login() {
               </label>
               <input
                 type="password"
+                placeholder="Password"
                 className="form-control"
                 id="password"
                 name="password"
@@ -73,7 +78,7 @@ function Login() {
               />
             </div>
 
-            <div className="d-grid gap-2">
+            <div className="d-grid gap-2 cum">
               <button
                 className="btn btn-primary"
                 type="submit"
@@ -85,12 +90,66 @@ function Login() {
           </form>
         </div>
 
-        <div className="card-footer text-muted text-center">
-          Don't have an Account? <Link to="/signup">Sign Up</Link> <br />
-          <Link to="/reset-password">Forgot Password? </Link>
+        <div className="card-footer text-">
+          {/* Don't have an Account? <Link to="/signup">Sign Up</Link> */}
+          <Link id="da-link" to="/reset-password">Forgot Password? </Link>
         </div>
       </div>
     </div>
+  </div>
+    // <div className="container mt-5">
+    //   <GuestHeader />
+    //   <div className="card custom-card-background">
+    //     <div className="card-header">
+    //       <h3 className="text-center">Log in</h3>
+    //     </div>
+
+    //     <div className="card-body">
+    //       <form onSubmit={handleSubmit}>
+    //         <div className="mb-3">
+    //           <label htmlFor="email" className="form-label">
+    //             Email:
+    //           </label>
+    //           <input
+    //             type="email"
+    //             className="form-control"
+    //             id="email"
+    //             name="email"
+    //             onChange={handleChange}
+    //           />
+    //         </div>
+
+    //         <div className="mb-3">
+    //           <label htmlFor="password" className="form-label">
+    //             Password:
+    //           </label>
+    //           <input
+    //             type="password"
+    //             className="form-control"
+    //             id="password"
+    //             name="password"
+    //             onChange={handleChange}
+    //           />
+    //         </div>
+
+    //         <div className="d-grid gap-2">
+    //           <button
+    //             className="btn btn-primary"
+    //             type="submit"
+    //             disabled={loading}
+    //           >
+    //             {loading ? "Logging in..." : "Login"}
+    //           </button>
+    //         </div>
+    //       </form>
+    //     </div>
+
+    //     <div className="card-footer text-muted text-center">
+    //       Don't have an Account? <Link to="/signup">Sign Up</Link> <br />
+    //       <Link to="/reset-password">Forgot Password? </Link>
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
 
