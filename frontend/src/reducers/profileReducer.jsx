3046769b191d999570e5ffe1e04b434db573cabc
Ikawari-s <lnmanese@student.hau.edu.ph>
2024-04-10 01:09:37 +0800

    comment pop up done, will do create post pop up next

--- frontend/src/reducers/profileReducer.jsx
+++ frontend/src/reducers/profileReducer.jsx
@@ -3,21 +3,11 @@ import {
     USER_PROFILE_SUCCESS,
     USER_PROFILE_FAIL
   } from '../constants/profileConstants';
-<<<<<<< HEAD
-  
-=======
-
->>>>>>> bf75d7074feeafd0e89f536c38d83d43ccfca557
   const initialState = {
     loading: false,
     userData: null,
     error: null
   };
-<<<<<<< HEAD
-  
-=======
-
->>>>>>> bf75d7074feeafd0e89f536c38d83d43ccfca557
   export const userProfileReducer = (state = initialState, action) => {
     switch (action.type) {
       case USER_PROFILE_REQUEST:
@@ -29,9 +19,4 @@ import {
       default:
         return state;
     }
-<<<<<<< HEAD
   };
-  
-=======
-  };
->>>>>>> bf75d7074feeafd0e89f536c38d83d43ccfca557
